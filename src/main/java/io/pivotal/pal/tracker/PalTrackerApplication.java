fb2e3e1e7ecc7c58ab26c253f1aa88489b1c2a7e
ubuntu <geetaarora2006>
2021-04-20 16:31:28 +0000

    Configuring an App

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class PalTrackerApplication {
+public class PalTrackerApplication {
 
     public static void main(String[] args )
     {
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private   String welcome_message;
+ public WelcomeController(@Value("${welcome.message}") String welcome_message) {
+     this.welcome_message = welcome_message;
+    }
 
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return welcome_message;
     }
+
+
 }
